470e5fa836a19d21694cf9574344fc45e43505e2
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-05-07 17:45:57 +0200

    split sliders, added showing, hidding buttons

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -3,56 +3,55 @@ import "../styles/home-page.scss";
 import Swiper, { Navigation } from "swiper";
 // import Swiper and modules styles
 import "swiper/css";
-// import "swiper/css/navigation";
+
+
+const commonConfig = {
+  modules: [Navigation],
+  direction: "horizontal",
+  slidesPerView: "auto",
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+};
 
 const swiperLoop = new Swiper(".nx-component__slider-content", {
-  modules: [Navigation],
-  direction: "horizontal",
-  effect: "coverflow",
+  ...commonConfig,
   loop: true,
-  slidesPerView: "auto",
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  /// jeśli slajd się zmienia, dodaje prevButton
+  ///
   on: {
-    /// jeśli slajd się zmienia, dodaje prevButton
-    ///
     slideChange: function (el) {
       const currentIndex = this.activeIndex;
-      
       const prevButton = this.el.querySelector(".prev");
       if(currentIndex > 0){
-        prevButton.dataset.prev = "1";
+        prevButton.dataset.button = "1";
+      } else {
+        prevButton.dataset.button = "0";
       }
     },
   },
 });
-
 
 const swiperNoLoop = new Swiper(".nx-component__slider-rated-content", {
-  modules: [Navigation],
-  direction: "horizontal",
-  effect: "coverflow",
-  slidesPerView: "auto",
-  grabCursor: true,
+  ...commonConfig,
   slidesOffsetAfter: 550,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
   on: {
     slideChange: function (el) {
       const currentIndex = this.activeIndex;
       const prevButton = this.el.querySelector(".prev");
-      console.log(prevButton)
-      if(currentIndex > 0){
-        prevButton.dataset.prev = "1";
+      const nextButton = this.el.querySelector(".next");
+      console.log(currentIndex)
+      if(currentIndex >= 9){
+        nextButton.dataset.button = "0";
+      } else if (currentIndex > 0) {
+        prevButton.dataset.button = "1";
       } else {
-        prevButton.dataset.prev = "0";
+        prevButton.dataset.button = "0";
       }
-    },
+    }
   },
 });
+
